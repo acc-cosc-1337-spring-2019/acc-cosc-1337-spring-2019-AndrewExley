d61d4081569caa4b067cfb7e07637de721f72fd3
AndrewExley <andrew.exley@g.austincc.edu>
2019-01-29 20:02:07 -0600

    Assingment 2

--- 01-basics_test/basics_test.cpp
+++ 01-basics_test/basics_test.cpp
@@ -14,8 +14,8 @@ TEST_CASE("Test sum_numbers", "[1,5,10]") {
 //5, 5 to equal 25
 //10,10 to equal 100
 
-TEST_CASE("Test sum_numbers", "[2,5,10]") {
-	REQUIRE(multiply_numbers(2, 2) == 4);
-	REQUIRE(multiply_numbers(5, 5) == 25);
+TEST_CASE("Test multiply_numbers", "[2,5,10]") {
+	REQUIRE(multiply_numbers(2, 4) == 8);
+	REQUIRE(multiply_numbers(3, 5) == 15);
 	REQUIRE(multiply_numbers(10, 10) == 100);
 }
