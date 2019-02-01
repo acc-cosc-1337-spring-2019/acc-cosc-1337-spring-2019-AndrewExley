aab7128dac330f7ada6ee52febec705c727941d9
AndrewExley <andrew.exley@g.austincc.edu>
2019-01-31 19:23:56 -0600

    Assignment 2a and 2b

--- examples/01_module/05_if/if.cpp
+++ examples/01_module/05_if/if.cpp
@@ -1,7 +1,21 @@
 //write include statement for if header
 
 //write code for the is_even function that returns true if num is even, false otherwise
-
+#include "if.h"
 
 //write code for function named overtime that accepts a double hours and returns true if 
 //hours over 40, false otherwise
+
+bool is_even(int num)
+{
+	return num % 2 == 0;
+}
+
+bool overtime(double hours)
+{
+	if (hours > 40)
+	{
+		return true;
+	}
+
+}
