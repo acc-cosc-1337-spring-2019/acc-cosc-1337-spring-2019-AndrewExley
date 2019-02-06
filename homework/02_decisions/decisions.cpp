b2faa65a9e91b85beb7875a9e56f50d20af71726
AndrewExley <andrew.exley@g.austincc.edu>
2019-02-06 16:19:56 -0600

    Homework 2

--- homework/02_decisions/decisions.cpp
+++ homework/02_decisions/decisions.cpp
@@ -1,5 +1,5 @@
 //write include statement for decisions header
-
+#include"decisions.h"
 //Write code for function get_grade_points that accepts a string letter_grade and returns 
 //the grade_points for as follows:
 //given grade "A" returns 4
@@ -8,14 +8,40 @@
 //given grade "D" returns 1
 //given grade "F" returns 0
 //another other value return -1
+int get_grade_points(std::string grade)
+{	
+	int grade_points;
+	if (grade == "A")
+		grade_points = 4;
+	else if (grade == "B")
+		grade_points = 3;
+	else if(grade == "C")
+		grade_points = 2;
+	else if(grade == "D")
+		grade_points = 1;
+	else if (grade == "F")
+		grade_points = 0;
+	else
+		grade_points = -1;
+	return grade_points;
 
-
-
-
-
-
+}
+ 
 //Write code for function calculate_gpa that accepts an int named credit_hours and
 //a double named credit_points.  The function returns the quotient of credit_points divided by
 //credit_hours. In the function account for division by zero by returning a -1.
+
+double calculate_gpa(int credit_hours, double credit_points)
+{
+	if (credit_hours == 0)
+		return -1;
+	return credit_points / credit_hours;
+}
+
+
+
+
+
+
 
 
--- homework/02_decisions/main.cpp
+++ homework/02_decisions/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include "decisions.h"
+
 //write include statements
 
 /*
@@ -21,9 +23,50 @@ Display:
 GPA 3.0
 
 */
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
 int main() 
 {
-	std::cout << "Hello" ;
+	string grade1;
+	string grade2;
+	string grade3;
+
+
+	cout << "Enter the first letter grade: ";
+	cin >> grade1;
+	cout << "Enter the letter second grade: ";
+	cin >> grade2;
+	cout << "Enter the letter third grade: ";
+	cin >> grade3;
+
+	int chour1;
+	int chour2;
+	int chour3;
+
+
+	cout << "Enter the credit hours for 1st course: ";
+	cin >> chour1;
+	cout << "Enter the credit hours for 2nd course: ";
+	cin >> chour2;
+	cout << "Enter the credit hours for 3rd course: ";
+	cin >> chour3;
+
+	double points1 = get_grade_points(grade1);
+	double points2 = get_grade_points(grade2);
+	double points3 = get_grade_points(grade3);
+
+	double total_points = points1 + points2 + points3;
+	int total_hours = chour1 + chour2 + chour3;
+	
+	double gpa = calculate_gpa(total_hours, total_points);
+	cout << "Total Points: " << total_points << endl;
+	cout << "Total Hours: " << total_hours << endl;
+	cout << "GPA: " << gpa << endl;
+	
+
+	system("pause");
 
 	return 0;
 }
