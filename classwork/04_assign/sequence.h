c20d7c057795918863b6e39ff67a64895c68b187
AndrewExley <47015463+AndrewExley@users.noreply.github.com>
2019-02-14 19:56:33 -0600

    Update sequence.h

--- classwork/04_assign/sequence.h
+++ classwork/04_assign/sequence.h
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<string>
+#include<vector>
+using std::string;
+using std::vector;
 /*
 Write prototype for void function display_vector that accepts a parameter const reference vector of strings
 */
+
 void display_vector(const vector<string>& values);
 
 /*
@@ -10,4 +14,4 @@ Write prototype for void function update_vector_element that accepts parameters 
 a string vector search_value, and a string replace_value.
 */
 
-void update_vector_element(vector<string>&values,std::string search_value, std::string replace_value);
+void update_vector_element(vector<string>&values, std::string search_value, std::string replace_value);
