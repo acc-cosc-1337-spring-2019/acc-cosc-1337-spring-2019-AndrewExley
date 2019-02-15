052963b9025083cf00df2e7c97ece81bd40e61d0
AndrewExley <47015463+AndrewExley@users.noreply.github.com>
2019-02-14 19:55:25 -0600

    Update main.cpp

--- classwork/04_assign/main.cpp
+++ classwork/04_assign/main.cpp
@@ -1,33 +1,25 @@
 #include<iostream>
 #include "sequence.h"
-#include<vector>
-#include<string>
-using std::string;
 using std::cout;
 using std::cin;
 /*
 Write code to create a vector of string names, add the values "John", "Mary", "Patty",
 "Sam" to the vector, call the function display_vector and pass the names vector as
-an argument, prompt user for a name to search for and a value to replace searched value with.
-Afterward, call the update_vector_element with the names vector and call the display_vector
-function.
-Don't worry about displaying a message for values that aren't found.  Assume user will
-give you a valid name.
+@@ -10,6 +17,17 @@ give you a valid name.
 */
 
 int main()
 {
-	
-	string values=vector<char> names = { "John","Mary","Patty","Sam" };
+
+	vector<string> names = { "John","Mary","Patty","Sam" };
 	display_vector(names);
-	string search, replace
+	string search, replace;
 
 	cout << "Enter a name to search for: ";
 	cin >> search;
 	cout<< "replace";
-	cin >> replace
-	update_vector(names, search, replace);
+	cin >> replace;
+	update_vector_element(names, search, replace);
 	display_vector(names);
 	return 0;
 }
-
