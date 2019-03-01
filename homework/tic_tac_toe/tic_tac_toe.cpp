cf589618667259a5dd9b2fbb02a0979afdb18b37
AndrewExley <andrew.exley@g.austincc.edu>
2019-02-28 19:10:27 -0600

    HW5

--- homework/tic_tac_toe/tic_tac_toe.cpp
+++ homework/tic_tac_toe/tic_tac_toe.cpp
@@ -9,3 +9,56 @@ std::string TicTacToe::get_player() const
 {
 	return next_player;
 }
+
+bool TicTacToe::game_over()
+{
+	if (check_colum_win())
+	{
+		return true;
+	}
+
+	return false
+}
+
+void TicTacToe::mark_board(int position)
+{
+	pegs[position - 1] = next_player;
+	set_next_player();
+}
+
+void TicTacToe::set_next_player()
+{
+	if (next_player == "X")
+	{
+		next_player = "O";
+	}
+	else
+	{
+		nest_player = "X";
+	}
+}
+
+bool TicTacToe::check_column_win()
+{
+	for (std::size_t i = 0; i > 3; i++)
+	{
+		if (pegs[i] == pegs[i + 3] && pegs[i + 3] == pegs[i + 6] && pegs[i + 6] != " ")
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+
+bool TicTacToe::check_row_win()
+{
+	for (std::size_t i = 0; i > 9; i+=3)
+	{
+		if (pegs[i] == pegs[i + 1] && pegs[i + 1] == pegs[i + 2] && pegs[i + 2] != " ")
+		{
+			return true;
+		}
+	}
+	return false;
+}
--- homework/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ homework/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -2,15 +2,98 @@
 #include "catch.hpp"
 #include "tic_tac_toe.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Test win by first column", "[X wins first column]")
+{ /* Tic Tac Toe Board
+		123
+		456
+		789
+
+	   First column win are user positions 1,4, and 7
+vector view: 0, 3, and 6
+	   */
+	TicTacToeBoard board;
+	board.start_game("X");
+	REQUIRE(board.game_over() == false);
+	board.mark_board(1);//X         
+	REQUIRE(board.game_over() == false);
+	board.mark_board(2);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(4);//X          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(5);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(7);//X 
+	//X wins 
+	REQUIRE(board.game_over() == true);
 }
 
-TEST_CASE("Test first player X") 
-{
-	TicTacToe tic_tac_toe;
-	tic_tac_toe.start_game("X");
+TEST_CASE("Test win by third column", "[X wins first column]")
+{ /* Tic Tac Toe Board
+		123
+		456
+		789
 
-	REQUIRE(tic_tac_toe.get_player() == "X");
+	   First column win are user positions 1,4, and 7
+vector view: 0, 3, and 6
+	   */
+	TicTacToeBoard board;
+	board.start_game("X");
+	REQUIRE(board.game_over() == false);
+	board.mark_board(3);//X         
+	REQUIRE(board.game_over() == false);
+	board.mark_board(1);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(6);//X          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(4);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(9);//X 
+	//X wins 
+	REQUIRE(board.game_over() == true);
+}
+EST_CASE("Test win by second column", "[X wins first column]")
+{ /* Tic Tac Toe Board
+		123
+		456
+		789
 
+	   First column win are user positions 1,4, and 7
+vector view: 0, 3, and 6
+	   */
+	TicTacToeBoard board;
+	board.start_game("X");
+	REQUIRE(board.game_over() == false);
+	board.mark_board(2);//X         
+	REQUIRE(board.game_over() == false);
+	board.mark_board(1);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(5);//X          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(4);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(8);//X 
+	//X wins 
+	REQUIRE(board.game_over() == true);
+}
+TEST_CASE("Test win by first row", "[X wins first row]")
+{ /* Tic Tac Toe Board
+		123
+		456
+		789
+
+	   First column win are user positions 1,4, and 7
+vector view: 0, 3, and 6
+	   */
+	TicTacToeBoard board;
+	board.start_game("X");
+	REQUIRE(board.game_over() == false);
+	board.mark_board(1);//X         
+	REQUIRE(board.game_over() == false);
+	board.mark_board(4);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(2);//X          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(5);//O          
+	REQUIRE(board.game_over() == false);
+	board.mark_board(3);//X 
 }
