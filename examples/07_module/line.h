edc0d7c7fcbacb29c7ca943f00b512f1245f7666
AndrewExley <andrew.exley@g.austincc.edu>
2019-03-28 18:48:18 -0500

    Classwork 3-28-19

--- examples/07_module/line.h
+++ examples/07_module/line.h
@@ -1,1 +1,10 @@
 //h
+#include "shape.h"
+#ifndef LINE_H
+#define LINE_H
+class Line : public Shape
+{
+public:
+	void draw();
+};
+#endif // !LINE_H
--- examples/07_module/main.cpp
+++ examples/07_module/main.cpp
@@ -1,9 +1,44 @@
 //main
+#include<memory>
+#include<iostream>
 #include "shape.h"
-int main() 
+#include"line.h"
+#include<vector>
+#include"circle.h"
+using std::vector;
+using std::cout;
+int main()
 {
-	Shape s;
-	s.draw();
+	//Shape s_abstract;(error) unable to create instance; pure virtual function makes an abstract class
 
+	Shape* line = new Line; //creates dynamic memory (heap)
+	line->draw();
+	delete line;
+	vector<Shape*> shapes{ new Line() , new Circle() };
+
+	for (auto& shape: shapes)
+	{
+	shape->draw();
+	}
+
+	cout << "\n";
+	for (auto*shape : shapes)
+	{
+		shape->draw();
+	}
+	cout << "\n";
+	//shape instance using a Smart Pointer
+	std::unique_ptr < Shape > l = std::make_unique<Line>();
+	l->draw();
+
+	//vector of shape pointers
+	vector <std::unique_ptr < Shape >> shaps;
+	shaps.push_back(std::make_unique< Line >());
+	shaps.push_back(std::make_unique< Circle >());
+
+	for (auto & shape : shapes)
+	{
+		shape->draw();
+	}
 	return 0;
 }
