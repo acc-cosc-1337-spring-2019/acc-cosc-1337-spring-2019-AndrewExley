6f78dbeeb301c7b5eaa934a017f90cc48e705f14
AndrewExley <andrew.exley@g.austincc.edu>
2019-04-23 19:16:50 -0500

    Notes 4-23-19

--- examples/11_module/01_ref_pointers/main.cpp
+++ examples/11_module/01_ref_pointers/main.cpp
@@ -1,5 +1,23 @@
+#include"ref_pointers.h"
+#include<iostream>
+
+
 int main() 
 {
+	int number = 10;
+	int*numberPtr = &number;
+
+	ref(number, numberPtr);
+
+	//Display value should be 100
+	std::cout << number;
+
+	std::cout << "Address of Number: " << &number << " value is " << number << "\n";
+	//a pointer can store the address of a variable
+	
+	std::cout << "Address of numberPtr:" << &numberPtr << "value stored at address poined to is" << numberPtr <<
+		"\n Deference numberPtr: " << *numberPtr;
+
 	
 	return 0;
 }
--- examples/11_module/02_dynamic_memory/dynamic_memory.cpp
+++ examples/11_module/02_dynamic_memory/dynamic_memory.cpp
@@ -1,1 +1,13 @@
+#include "dynamic_memory.h"
+#include <iostream>
 //
+
+void allocate_use_release()
+{
+	int stack_num{ 5 };//stack vaiable
+	int*num = new int(5);//heap, data variable
+	
+	std::cout << *num; //USE
+	delete num;//RELEASE
+	num = nullptr;
+}
