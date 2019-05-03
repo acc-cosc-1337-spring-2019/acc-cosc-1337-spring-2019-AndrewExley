7c9a659c5aa07ea91b32928b2b947907a1051ca3
AndrewExley <andrew.exley@g.austincc.edu>
2019-05-02 19:16:27 -0500

    Classwork 5-2-19

--- examples/11_module/06_destructors/main.cpp
+++ examples/11_module/06_destructors/main.cpp
@@ -1,6 +1,23 @@
-int main() 
+#include "destructor.h"
+
+Destructor create_class();
+int main()
 {
+	Destructor d(10);
+	Destructor c(5);
+	Destructor a = d;//create class copy; copy constructor executes
+	Destructor b(d);//executes copy constructor
 
+	c = d;// executes copy assignment
 
+	d.set_value(5);
+	a.set_ptr_num(100);
+	int x = 5;
+	int&y = x;
+	int*z = &x;
 	return 0;
+}
+Destructor create_class()
+{
+	return Destructor(5);
 }
--- examples/11_module/09_rule_of_5/rule_of_five.h
+++ examples/11_module/09_rule_of_5/rule_of_five.h
@@ -1,1 +1,18 @@
-//
+class RuleOfFive
+{
+public:
+	RuleOfFive(int n) : num(n) { ptr_num = new int(n); }
+	RuleOfFive(const RuleOfFive& d); //copy constructor - RULE OF 3
+	RuleOfFive& operator=(RuleOfFive& d); //coppy assignment - RULE OF 3
+	RuleOfFive(RuleOfFive&& r);
+	RuleOfFive& operator =(RuleOfFive&& r);
+	~RuleOfFive(); // RULE OF 3
+	void set_value(int n);
+	int get_value();
+
+	void set_ptr_num(int n);
+	int* get_ptr_num();
+private:
+	int num;
+	int* ptr_num;
+};
