799db9760c77d62707662a00fc5addcee7c4bc15
AndrewExley <andrew.exley@g.austincc.edu>
2019-05-09 18:56:19 -0500

    5-9-19

--- examples/12_module/10_arrays_dyn_3/report.cpp
+++ examples/12_module/10_arrays_dyn_3/report.cpp
@@ -1,1 +1,28 @@
 //
+//
+#include "report.h"
+#include<iostream>
+Report::Report()
+{
+	//read from, file or database
+	for (int i = 0; i < 5; i++)
+	{
+		accounts[i] = new BankAccount(i * 100);
+	}
+}
+void Report::display()
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << accounts[i]->get_balance() << "\n";
+	}
+}
+Report::~Report()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "Clear element \n";
+		delete acounts[i];
+		accounts[i] = nullptr;
+	}
+}
